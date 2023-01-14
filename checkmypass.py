10757d9054412a4dccb14de05ab915d91f8c43bd
Admin <admin@Admins-MacBook-Pro.local>
2023-01-13 16:09:37 -0800

    first commit

---
+++ checkmypass.py
@@ -0,0 +1,15 @@
+import requests
+import hashlib
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+def pwned_api_check(password):
+    # sha1password = hashlib.sha1(password.encode())
+    pass
+
+request_api_data('CDBF')

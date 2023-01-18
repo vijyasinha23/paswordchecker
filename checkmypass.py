7277f89ec05a119150d2c88ab509424c456cf9a4
Admin <admin@Admins-MacBook-Pro.local>
2023-01-18 11:57:24 -0800

    modified code

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
@@ -8,8 +9,31 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
     return res
 
-def pwned_api_check(password):
-    # sha1password = hashlib.sha1(password.encode())
-    pass
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
 
-request_api_data('CDBF')
+
+def pwned_api_check(password):
+    # check password if it exists in API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first_5_char, tail = sha1password[:5], sha1password[5: ]
+    response = request_api_data(first_5_char)
+    return get_password_leaks_count(response, tail)
+    # sha1password = hashlib.sha1(password.encode())
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times')
+        else:
+            print(f'{password} not found')
+    return 'done'
+
+if __name__ == '__main__':
+   sys.exit(main(sys.argv[1:]))
+
